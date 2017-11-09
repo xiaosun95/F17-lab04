0c3a87942e4542247d5085aa07e3ceb687625236
xiaosun95 <xiaosun@umail.ucsb.edu>
2017-11-08 16:54:43 -0800

    update

--- src/edu/ucsb/cs56/drawings/pconrad/simple/PictureComponent.java
+++ src/edu/ucsb/cs56/drawings/pconrad/simple/PictureComponent.java
@@ -13,7 +13,7 @@ import java.awt.geom.Rectangle2D;
    A component that draws a Picture by Phill Conrad
    
    @author Phill Conrad (original drawing)
-   @author @@@ ADD YOUR NAME (fixed the snowmans's head)
+   @author Xiao (Chales Sun)Sun (fixed the snowmans's head)
    @version for UCSB CS56, W16
 */
 
@@ -89,13 +89,21 @@ public class PictureComponent extends JComponent
         g2.draw(snowManMiddleCircle);
 	
         // @@@ ADD CODE HERE TO DRAW THE TOP CIRCLE
+ Circle snowManTopCircle =
+            new Circle
+            (
+             snowManCenterBottomX,
+             snowManCenterBottomY - bottomRadius * 3 - topRadius,
+             topRadius
+             );
+        g2.draw(snowManTopCircle);
 
 
 
         // @@@ FINALLY, SIGN AND LABEL YOUR DRAWING
 	// @@@ 20, 20 are suggested coordinates, but you may change them
 
-        // g2.drawString("Description and your name go here", 20,20);
+         g2.drawString("Xiao (Charles) Sun", 20,20);
 	
     }
 }
